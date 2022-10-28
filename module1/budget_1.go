0b919837455b88e6db86f49ac2b8d6f365ed1a4a
morrismao <shady28612458@gmail.com>
2022-10-28 11:17:13 +0800

    setup

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -2,6 +2,12 @@ package module1
 
 // Budget stores budget information
 type Budget struct {
+	Max  float32
+	Item []Item
 }
 
 // Item stores item information
+type Item struct {
+	Description string
+	Price       float32
+}
